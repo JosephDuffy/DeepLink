17eb5e4197c65ba4d36a06129318a684addcbd05
Joseph Duffy <commits@josephduffy.co.uk>
2023-10-02 22:15:31 +0100

    Add a custom type for Overcast’s iTunes ID to support `init(url:)`

--- Sources/OvercastDeepLink/OvercastPodcastPageUniversalLink.swift
+++ Sources/OvercastDeepLink/OvercastPodcastPageUniversalLink.swift
@@ -10,12 +10,17 @@ public struct OvercastPodcastPageUniversalLink: DeepLink {
     public let host = "overcast.fm"
 
     /// The Apple-provided identifier for the podcast.
-    public var iTunesId: Int
+    public var iTunesId: Int {
+        get {
+            path.iTunesId
+        }
+        set {
+            path.iTunesId = newValue
+        }
+    }
 
     @PathItem
-    private var path: String {
-        "itunes\(iTunesId)"
-    }
+    private var path: OvercastiTunesID
 
     /// The name of the podcast, which is automatically appended to the URL by
     /// the Overcast server if not provided or incorrect.
@@ -25,7 +30,33 @@ public struct OvercastPodcastPageUniversalLink: DeepLink {
     public var name: String?
 
     public init(iTunesId: Int, name: String? = nil) {
-        self.iTunesId = iTunesId
+        self.path = OvercastiTunesID(iTunesId: iTunesId)
         self.name = name
+    }
+}
+
+/// An iTunes ID as used by Overcast.
+///
+/// The conforms to `LosslessStringConvertible` which will allow
+/// `generateInitWithURL` to be set to `true` once support for optional
+/// `@PathItem`s is support when it's the last path item.
+private struct OvercastiTunesID: Hashable, LosslessStringConvertible, Sendable {
+    private static let prefix = "itunes"
+
+    public var iTunesId: Int
+
+    public var description: String {
+        "\(Self.prefix)\(iTunesId)"
+    }
+
+    public init(iTunesId: Int) {
+        self.iTunesId = iTunesId
+    }
+
+    public init?(_ description: String) {
+        guard description.hasPrefix(Self.prefix) else { return nil }
+        let possibleId = description.dropFirst(Self.prefix.count)
+        guard let iTunesId = Int(possibleId) else { return nil }
+        self.iTunesId = iTunesId
     }
 }
